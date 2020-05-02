bf1b11a4957fdd53d287884e5c84a43f50ed1e18
Boatle <32166344+Boatle@users.noreply.github.com>
2020-05-02 18:27:53 +0100

    Updated icons
    Map zoom z-index fix too.

--- scripts/tovemapleaflet.js
+++ scripts/tovemapleaflet.js
@@ -1,9 +1,9 @@
-var map = L.map('tove-valley-map').setView([52.124655, -0.963180], 13);
+var map = L.map('tove-valley-map').setView([52.125376, -0.962026], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([52.124655, -0.963180]).addTo(map)
+L.marker([52.125376, -0.962026]).addTo(map)
     // .bindPopup('Tove Valley Cottages<br>')
     .openPopup();
